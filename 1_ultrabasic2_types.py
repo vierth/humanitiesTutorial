bfab40f2e76ea774fd740d71fefdc9c4f487d722
Paul Vierthaler <p.a.vierthaler@hum.leidenuniv.nl>
2018-07-19 11:57:07 +0200

    add comment file

---
+++ 1_ultrabasic2_types.py
@@ -0,0 +1,28 @@
+# The basic data types in Python
+
+# String:
+"This is a string"
+
+'This is also a string'
+
+# Integer
+1
+2
+100000
+824825
+
+# Float
+1.0
+2.0
+3.14159
+-120.3
+
+# Boolean
+True
+False
+
+# Nonetype
+None # This evaluates to False
+
+# This prints to console
+print("hello, world")
